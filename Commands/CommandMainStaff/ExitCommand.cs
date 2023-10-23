44b74c361c166f1f2397c5df3b2d5135c83a2ca5
Trình LÊ <T20110408@outlook.com.vn>
2023-10-23 11:23:32 +0700

    push test 1

--- Commands/CommandMainStaff/ExitCommand.cs
+++ Commands/CommandMainStaff/ExitCommand.cs
@@ -9,7 +9,7 @@ namespace DevCoffeeManagerApp.Commands.CommandMainStaff
 {
     public class ExitCommand : CommandBase
     {
-        public override bool CanExecute(object parameter)
+        public override bool CanExecute(object parameter) 
         {
             return true;
         }
