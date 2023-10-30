818c7d6b86863046ce58949d1518694114079ca2
Dhyun27sp <dinhtanphuchuy@gmail.com>
2023-10-30 21:58:11 +0700

    update bookTable

---
+++ Commands/CommandTable/CommandBook.cs
@@ -0,0 +1,41 @@
+﻿using DevCoffeeManagerApp.Models;
+using DevCoffeeManagerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace DevCoffeeManagerApp.Commands.CommandTable
+{
+    public class CommandBook : CommandBase
+    {
+        private TableViewModel Viewmodeltable;
+        public CommandBook(TableViewModel viewmodeltable)
+        {
+            Viewmodeltable = viewmodeltable;
+        }
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public override void Execute(object parameter)
+        {
+            if (parameter is ListView listView)
+            {
+                // Lấy danh sách các mục đã chọn từ SelectedItems
+                ObservableCollection<TableModel> selectedItems = new ObservableCollection<TableModel>(listView.SelectedItems.Cast<TableModel>());
+
+                // Bây giờ, `selectedItems` chứa danh sách các hàng đã chọn
+                // Bạn có thể duyệt qua danh sách này để làm bất kỳ thao tác nào bạn cần
+                foreach (TableModel selectedItem in selectedItems)
+                {
+                    MessageBox.Show(selectedItem.No_.ToString());
+                }
+            }
+        }
+    }
+}
