c138b2530cdffca4d5221da181ed367960377127
TrinhGitHub <lecongtrinhbhxh@gmail.com>
2023-12-03 15:14:10 +0700

    function separation in filter

--- Commands/CommandOrder/FilterByTypeCommand.cs
+++ Commands/CommandOrder/FilterByTypeCommand.cs
@@ -4,6 +4,7 @@ using DevCoffeeManagerApp.Store;
 using DevCoffeeManagerApp.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Xml.Linq;
 
 namespace DevCoffeeManagerApp.Commands.CommandOrder
 {
@@ -34,193 +36,51 @@ namespace DevCoffeeManagerApp.Commands.CommandOrder
         {
             orderFoodViewModel.Dishs = filter(orderFoodViewModel.AllDishsVariable, orderFoodViewModel.types_dish_search, orderFoodViewModel.Type, orderFoodViewModel.Type_Special);
         }
-            public List<DishModel> filter(List<DishModel> AllDishsVariable, string types_dish_search, string Type, string Type_Special)
+        public List<DishModel> filter(List<DishModel> AllDishsVariable, string types_dish_search, string Type, string Type_Special)// hàm fiter cho search, loại món, loại món đặt biệt
+        {
+            List<DishModel> Dishs_Search = new List<DishModel>();
+            if (types_dish_search != "")
             {
-                List<DishModel> Dishs_Search = new List<DishModel>();// Danh sách món nạp mấy món đủ điều kiện vào
-                if (types_dish_search != "")// nếu mà search nó khác rỗng
+                foreach (var item in AllDishsVariable)
                 {
-                    foreach (var item in AllDishsVariable) // lọc danh sách tất cả các món
+                    bool contains = Dish_have_key(item, types_dish_search);
+                    if (contains)// trường hợp key có trong món ăn
                     {
-                        bool contains = false;
-                        if (HasDiacriticMarksNormalization(types_dish_search) == true)
+                        if (Type != "All Dishs")
                         {
-                            if (wordsign_notsign(types_dish_search) == true)
+                            if (item.category == Type)
                             {
-                            string itemconvertUnicode = ConvertString(item.dish_name).Replace(" ", "");
-                            string types_dish_searchconvertUnicode = ConvertString(types_dish_search).Replace(" ", "");
-                            bool contains_not_sign = itemconvertUnicode.Contains(types_dish_searchconvertUnicode);
-                            contains = contains_not_sign;
+                                AddToResultList(ref Dishs_Search, item, Type_Special);
                             }
-                            else
-                            {
-                                bool contains_sign = item.dish_name.Replace(" ", "").IndexOf(types_dish_search.Replace(" ", ""), StringComparison.OrdinalIgnoreCase) >= 0;
-                                contains = contains_sign;
-                                if(contains_sign == false)
-                                {
-                                    string itemconvertUnicode = ConvertString(item.dish_name).Replace(" ", "");
-                                    string types_dish_searchconvertUnicode = ConvertString(types_dish_search).Replace(" ", "");
-                                    bool contains_not_sign = itemconvertUnicode.Contains(types_dish_searchconvertUnicode);
-                                    contains = contains_not_sign;
-                                }
-                            }
-                        }
+                        } 
                         else
                         {
-                            string itemconvertUnicode = ConvertString(item.dish_name).Replace(" ", "");
-                            string types_dish_searchconvertUnicode = ConvertString(types_dish_search).Replace(" ", "");
-                            bool contains_not_sign = itemconvertUnicode.Contains(types_dish_searchconvertUnicode);
-                            contains = contains_not_sign;
+                            AddToResultList(ref Dishs_Search, item, Type_Special);
                         }
-                        
-                        if (contains)
-                        {
-                            if (Type != "All Dishs")
-                            {
-                                if (item.category == Type) 
-                                {
-                                    if (Type_Special == "Discounted")
-                                {
-                                    if (item.SaleDish == true)
-                                    {
-                                        Dishs_Search.Add(item);
-                                    }
-                                }
-                                else if (Type_Special == "New Dish")
-                                {
-                                    if (item.newDish == true)
-                                    {
-                                        Dishs_Search.Add(item);
-                                    }
-                                }
-                                else if (Type_Special == "Hot Dish")
-                                {
-                                    if (item.HotDish == true)
-                                    {
-                                        Dishs_Search.Add(item);
-                                    }
-                                }
-                                else
-                                {
-                                   Dishs_Search.Add(item);
-                                }
-                                }
-                            } // kiểm tra nếu nó không phải loại ko phải tất cả
-                            else
-                            {
-                                if (Type_Special == "Discounted")
-                                {
-                                    if (item.SaleDish == true)
-                                    {
-                                        Dishs_Search.Add(item);
-                                    }
-                                }
-                                else if (Type_Special == "New Dish")
-                                {
-                                    if (item.newDish == true)
-                                    {
-                                        Dishs_Search.Add(item);
-                                    }
-                                }
-                                else if (Type_Special == "Hot Dish")
-                                {
-                                    if (item.HotDish == true)
-                                    {
-                                        Dishs_Search.Add(item);
-                                    }
-                                }
-                                else
-                                {
-                                   Dishs_Search.Add(item);
-                                }
-                            }
-                        }
-                        
                     }
-                    if (Dishs_Search.Count == 0)
-                    {
-                        return null;
-                    }
-                }
-                else // search là rỗng  
-                {
-                    AllDishsVariable = orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type);
-                    if (Type != "All Dishs")
-                    {
-                        if (Type_Special == "Discounted")
-                        {
-                            List<DishModel> combine = new List<DishModel>();
-                            List<DishModel> notdishcount = new List<DishModel>();
-                            notdishcount = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
-                            combine.AddRange(Dishs_Search);
-                            combine.AddRange(notdishcount);
-                            return sort_special_type_comesfirst(combine, Type_Special);
-                        }
-                        else if (Type_Special == "New Dish")
-                        {
-                            List<DishModel> combine = new List<DishModel>();
-                            List<DishModel> notdishnew = new List<DishModel>();
-                            notdishnew = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
-                            combine.AddRange(Dishs_Search);
-                            combine.AddRange(notdishnew);
-                            return sort_special_type_comesfirst(combine, Type_Special);
-                        }
-                        else if (Type_Special == "Hot Dish")
-                        {
-                            List<DishModel> combine = new List<DishModel>();
-                            List<DishModel> notdishHot = new List<DishModel>();
-                            notdishHot = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
-                            combine.AddRange(Dishs_Search);
-                            combine.AddRange(notdishHot);
-                            return sort_special_type_comesfirst(combine, Type_Special);
-                        }
-                        return AllDishsVariable;
-                    }
-                    else
-                    {
-                        if (Type_Special == "Discounted")
-                        {
-                            List<DishModel> combine = new List<DishModel>();
-                            List<DishModel> notdishcount = new List<DishModel>();
-                            notdishcount = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
-                            combine.AddRange(Dishs_Search);
-                            combine.AddRange(notdishcount);
-                            return sort_special_type_comesfirst(combine, Type_Special);
-                        }
-                        else if (Type_Special == "New Dish")
-                        {
-                            List<DishModel> combine = new List<DishModel>();
-                            List<DishModel> notdishnew = new List<DishModel>();
-                            notdishnew = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
-                            combine.AddRange(Dishs_Search);
-                            combine.AddRange(notdishnew);
-                            return sort_special_type_comesfirst(combine, Type_Special);
-                        }
-                        else if (Type_Special == "Hot Dish")
-                        {
-                            List<DishModel> combine = new List<DishModel>();
-                            List<DishModel> notdishHot = new List<DishModel>();
-                            notdishHot = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
-                            combine.AddRange(Dishs_Search);
-                            combine.AddRange(notdishHot);
-                            return sort_special_type_comesfirst(combine, Type_Special);
-                        }
-                        return AllDishsVariable;
 
                 }
-                    return orderFoodViewModel.LoadOnlydiscountfortype(AllDishsVariable, Type_Special, Type);
+                if (Dishs_Search.Count == 0)//trương hợp key ko có trong món nào
+                {
+                    return null;
                 }
-                return Dishs_Search;
             }
-        private List<DishModel> DeleteduplicatesDish(List<DishModel> ListOnlyDisCont, List<DishModel> ListonlyType)
+            else // trương hợ search rỗng 
+            {
+                AllDishsVariable = orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type);
+                return sort_special_Dish_first(AllDishsVariable, Dishs_Search, Type_Special, Type);
+            }
+            return Dishs_Search;
+        }
+        private List<DishModel> DeleteduplicatesDish(List<DishModel> ListOnlyDisCont, List<DishModel> ListonlyType)// xóa món trùng nhau
         {
             List<DishModel> result = new List<DishModel>();
-
             result = ListonlyType.Where(item => !ListOnlyDisCont.Contains(item)).ToList();
             return result;
         }
         private List<DishModel> sort_special_type_comesfirst(List<DishModel> special_type_comesfirst, string special_type)
         {
-            List<DishModel> sortedList = new List<DishModel>();
+            List<DishModel> sortedList = new List<DishModel>(); //sắp xếp món đặt biệt lên trước 
             if (special_type == "Discounted")
             {
                 sortedList = special_type_comesfirst
@@ -243,7 +103,7 @@ namespace DevCoffeeManagerApp.Commands.CommandOrder
             return sortedList;
             
         }
-        private string ConvertString(string stringInput)
+        private string ConvertString(string stringInput)// hàm chuyển đổi chữ tiếng việt thành không đấu viết thường
         {
             stringInput = stringInput.ToLower();
             string convert = "ĂÂÀẰẦÁẮẤẢẲẨÃẴẪẠẶẬỄẼỂẺÉÊÈỀẾẸỆÔÒỒƠỜÓỐỚỎỔỞÕỖỠỌỘỢƯÚÙỨỪỦỬŨỮỤỰÌÍỈĨỊỲÝỶỸỴĐăâàằầáắấảẳẩãẵẫạặậễẽểẻéêèềếẹệôòồơờóốớỏổởõỗỡọộợưúùứừủửũữụựìíỉĩịỳýỷỹỵđ";
@@ -254,17 +114,9 @@ namespace DevCoffeeManagerApp.Commands.CommandOrder
             }
             return stringInput;
         }
-        private bool HasDiacriticMarksNormalization(string inputStr)// chữ có dấu
+        private bool wordsign_notsign(string name)// hàm kiểm tra cụm từ vừa dấu vừa không
         {
-            // Chuyển đổi chuỗi sang UnicodeNormalizationForm.FormD
-            string normalizedStr = inputStr.Normalize(NormalizationForm.FormD);
 
-            // Lọc ra các ký tự không phải là ký tự chữ cái hoặc số
-            return normalizedStr.Any(c => CharUnicodeInfo.GetUnicodeCategory(c) == UnicodeCategory.NonSpacingMark);
-        }
-
-        private bool wordsign_notsign(string name)
-        {
             int index_word_have_sign = 0;
             int index_word_not_have_sign = 0;
             string[] words = name.Split(' ');
@@ -272,7 +124,8 @@ namespace DevCoffeeManagerApp.Commands.CommandOrder
             // In từng từ ra màn hình
             foreach (string word in words)
             {
-                if (HasDiacriticMarksNormalization(word))
+                bool word_have_sign = word.Normalize(NormalizationForm.FormD).Any(c => CharUnicodeInfo.GetUnicodeCategory(c) == UnicodeCategory.NonSpacingMark);
+                if (word_have_sign)
                 {
                     index_word_have_sign++;
                 }
@@ -286,6 +139,95 @@ namespace DevCoffeeManagerApp.Commands.CommandOrder
                 return true;
             }
             return false;
+        }
+
+        private bool Dish_have_key(DishModel item,string key)//hàm kiểm tra đầu vào key
+        {
+            bool word_have_sign = key.Normalize(NormalizationForm.FormD).Any(c => CharUnicodeInfo.GetUnicodeCategory(c) == UnicodeCategory.NonSpacingMark);
+            
+            bool contains = false;
+            if (word_have_sign == true)
+            {
+                if (wordsign_notsign(key) == true)
+                {
+                    string itemconvertUnicode = ConvertString(item.dish_name).Replace(" ", "");
+                    string types_dish_searchconvertUnicode = ConvertString(key).Replace(" ", "");
+                    bool contains_not_sign = itemconvertUnicode.Contains(types_dish_searchconvertUnicode);
+                    contains = contains_not_sign;
+                }
+                else
+                {
+                    bool contains_sign = item.dish_name.Replace(" ", "").IndexOf(key.Replace(" ", ""), StringComparison.OrdinalIgnoreCase) >= 0;
+                    contains = contains_sign;
+                    if (contains_sign == false && key.Count() > 1)
+                    {
+                        string itemconvertUnicode = ConvertString(item.dish_name).Replace(" ", "");
+                        string types_dish_searchconvertUnicode = ConvertString(key).Replace(" ", "");
+                        bool contains_not_sign = itemconvertUnicode.Contains(types_dish_searchconvertUnicode);
+                        contains = contains_not_sign;
+                    }
+                }
+            }
+            else
+            {
+                string itemconvertUnicode = ConvertString(item.dish_name).Replace(" ", "");
+                string types_dish_searchconvertUnicode = ConvertString(key).Replace(" ", "");
+                bool contains_not_sign = itemconvertUnicode.Contains(types_dish_searchconvertUnicode);
+                contains = contains_not_sign;
+            }
+            return contains;
+        }
+
+        private void AddToResultList(ref List<DishModel> Dishs_Search, DishModel item, string Type_Special)
+        {
+            if (Type_Special == "Discounted" && item.SaleDish)
+            {
+                Dishs_Search.Add(item);
+            }
+            else if (Type_Special == "New Dish" && item.newDish)
+            {
+                Dishs_Search.Add(item);
+            }
+            else if (Type_Special == "Hot Dish" && item.HotDish)
+            {
+                Dishs_Search.Add(item);
+            }
+            else
+            {
+                Dishs_Search.Add(item);
+            }
+        }
+
+        private List<DishModel> sort_special_Dish_first(List<DishModel> AllDishsVariable, List<DishModel> Dishs_Search,string Type_Special,string Type)
+        {
+            if (Type_Special == "Discounted")
+            {
+                List<DishModel> combine = new List<DishModel>();
+                List<DishModel> notdishcount = new List<DishModel>();
+                notdishcount = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
+                combine.AddRange(Dishs_Search);
+                combine.AddRange(notdishcount);
+                return sort_special_type_comesfirst(combine, Type_Special);
+            }
+            else if (Type_Special == "New Dish")
+            {
+                List<DishModel> combine = new List<DishModel>();
+                List<DishModel> notdishnew = new List<DishModel>();
+                notdishnew = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
+                combine.AddRange(Dishs_Search);
+                combine.AddRange(notdishnew);
+                return sort_special_type_comesfirst(combine, Type_Special);
+            }
+            else if (Type_Special == "Hot Dish")
+            {
+                List<DishModel> combine = new List<DishModel>();
+                List<DishModel> notdishHot = new List<DishModel>();
+                notdishHot = DeleteduplicatesDish(Dishs_Search, orderFoodViewModel.LoadDishWithType(AllDishsVariable, Type));
+                combine.AddRange(Dishs_Search);
+                combine.AddRange(notdishHot);
+                return sort_special_type_comesfirst(combine, Type_Special);
+            }
+            return AllDishsVariable;
         }
     }
 }
