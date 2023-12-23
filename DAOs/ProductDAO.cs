6d480ee5cca49387240d793a654b9837f707d864
Dhyun27sp <dinhtanphuchuy@gmail.com>
2023-12-23 21:05:23 +0700

    up date file

---
+++ DAOs/ProductDAO.cs
@@ -0,0 +1,42 @@
+﻿using DevCoffeeManagerApp.Config;
+using DevCoffeeManagerApp.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoffeeManagerApp.DAOs
+{
+    public class ProductDAO
+    {
+        private IMongoCollection<ProductModel> collection;
+
+        public ProductDAO()
+        {
+            IMongoDatabase db = ConnectionMongoDB.getdatabase();
+            collection = db.GetCollection<ProductModel>("Product");
+        }        
+
+        public ObservableCollection<ProductModel> GetAllProducts()
+        {
+            List<ProductModel> customers = collection.Find(new BsonDocument()).ToList();
+            return new ObservableCollection<ProductModel>(customers);
+        }
+
+        public void CreateCustomer(ProductModel product)
+        {
+            collection.InsertOne(product);
+        }
+
+        //public void UpdateCustomer(ProductModel customer)
+        //{
+        //    var phone_number = Builders<ProductModel>.Filter.Eq("phone_number", customer.phone_number);
+        //    collection.ReplaceOne(phone_number, customer);
+        //}        
+
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevCoffeeManagerApp.Models
+{
+    [Serializable, BsonIgnoreExtraElements]
+    public class ProductModel
+    {
+        [BsonElement("product_name"), BsonRepresentation(BsonType.String)]
+        public String Product_name { get; set; }
+
+        [BsonElement("product_id"), BsonRepresentation(BsonType.String)]
+        public String Product_id { get; set; }
+               
+        [BsonElement("stock"), BsonRepresentation(BsonType.Int32)]
+        public int Stock { get; set; }
+
+        [BsonElement("unit"), BsonRepresentation(BsonType.String)]
+        public String Unit { get; set; }
+
+        [BsonElement("EXP"), BsonRepresentation(BsonType.DateTime)]
+        public DateTime? EXP_date { get; set; }
+
+        [BsonElement("manufacturer"), BsonRepresentation(BsonType.String)]
+        public String Manufacturer { get; set; }
+        public ProductModel(ProductModel model)
+        {
+            this.Product_name = model.Product_name;
+            this.Product_id = model.Product_id;            
+            this.Stock = model.Stock;
+            this.Unit = model.Unit;
+            this.EXP_date = model.EXP_date;
+            this.Manufacturer = model.Manufacturer;
+        }
+        public ProductModel()
+        {
+        }
+    }
+}
