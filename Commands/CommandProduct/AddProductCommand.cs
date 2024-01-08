102c3d9d1cde843f35e4cd744f92e0f7356c8551
DucTrungDo <ducdt2192@gmail.com>
2024-01-08 23:46:09 +0700

    fix add duplicate product

--- Commands/CommandProduct/AddProductCommand.cs
+++ Commands/CommandProduct/AddProductCommand.cs
@@ -29,11 +29,11 @@ namespace DevCoffeeManagerApp.Commands.CommandProduct
             ProductModel model = new ProductModel(adminProductViewModel.newproduct);
             if (model.Product_name == null || model.Product_name == "" || model.Unit == null || model.Unit == "")
             {
-                MessageBox.Show("Hàm hoá rõng không thể thêm");
+                MessageBox.Show("Hàng hóa rỗng, không thể thêm");
                 return;
             }
             ProductModel existmodel = productDAO.GetProductbyName(model.Product_name);
-            if (existmodel == null)
+            if (existmodel != null)
             {
                 MessageBox.Show("Đã có sản phẩm này");
                 return;
